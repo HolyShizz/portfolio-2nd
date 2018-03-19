7969ff4a4e1b1bc750098fd32b1f33f76dea06fd
HolyShizz <filippsoltys@gmail.com>
2018-03-19 02:33:42 +0200

    123

--- src/js/vendor/main.js
+++ src/js/vendor/main.js
@@ -1,23 +1,26 @@
-$(document).ready(() => {
-  $('.top-slider').slick({
-    arrows:true,
-    speed:500,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    infinite: true
+newFunction();
+function newFunction() {
+  $(document).ready(() => {
+    $('.top-slider').slick({
+      arrows: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      infinite: true,
+    });
+    $('.bot-slider').slick({
+      arrows: true,
+      speed: 500,
+      infinite: true,
+      appendArrows: '.bot-slider-nav',
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="img/navl.png"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"><img src="img/navr.png"></button>',
+    });
   });
-  $('.bot-slider').slick({
-    arrows:true,
-    speed:500,
-    infinite: true,
-    appendArrows: '.bot-slider-nav',
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="img/navl.png"></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><img src="img/navr.png"></button>'
+  $('div.pure-u-1-4').click(function (e) {
+    e.preventDefault();
+    $('div.pure-u-1-4').removeClass('active-menu');
+    $(this).addClass('active-menu');
   });
-});
+}
 
-$('div.pure-u-1-4').click(function (e) {
-  e.preventDefault();
-  $('div.pure-u-1-4').removeClass('active-menu');
-  $(this).addClass('active-menu');
-});

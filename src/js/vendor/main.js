6d9bfcfc88e0f1995b129671c8f6d79c3bb2e85c
HolyShizz <filippsoltys@gmail.com>
2018-03-29 02:10:21 +0300

    2903

--- src/js/vendor/main.js
+++ src/js/vendor/main.js
@@ -32,13 +32,17 @@ function newFunction() {
       prevArrow: '<button class="slick-arrow slick-prev"><img src="img/navl.png"></button>',
       nextArrow: '<button class="slick-arrow slick-next"><img src="img/navr.png"></button>',
     });
+
+    $('div.pure-u-1-4').click(function (e) {
+      $('div.pure-u-1-4').removeClass('active-menu');
+      $(this).addClass('active-menu');
+    });
+
+    
+  
   });
+};
 
 
-  $('div.pure-u-1-4').click(function (e) {
-    e.preventDefault();
-    $('div.pure-u-1-4').removeClass('active-menu');
-    $(this).addClass('active-menu');
-  });
-}
 
+ 

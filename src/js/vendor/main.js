624b33a72b6f292b30d62229fb512134601c95f8
HolyShizz <filippsoltys@gmail.com>
2018-03-29 13:38:54 +0300

    release

--- src/js/vendor/main.js
+++ src/js/vendor/main.js
@@ -1,48 +1,41 @@
-newFunction();
-function newFunction() {
-  $(document).ready(() => {
+$(document).ready(() => {
     $('.top-slider').slick({
-      arrows: true,
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      infinite: true,
-      adaptiveHeight: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            arrows: false,
-            dots: true,
-            speed: 300,
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            infinite: true,
-            adaptiveHeight: true,
-            infinite: true
-          },
-        },
-      ],
+        arrows: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        adaptiveHeight: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                arrows: false,
+                dots: true,
+                speed: 300,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                adaptiveHeight: true,
+                infinite: true
+            },
+        }, ],
     });
+
     $('.bot-slider-content').slick({
-      arrows: true,
-      speed: 500,
-      infinite: true,
-      appendArrows: '.bot-slider-nav',
-      prevArrow: '<button class="slick-arrow slick-prev"><img src="img/navl.png"></button>',
-      nextArrow: '<button class="slick-arrow slick-next"><img src="img/navr.png"></button>',
+        arrows: true,
+        speed: 500,
+        infinite: true,
+        appendArrows: '.bot-slider-nav',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="img/navl.png"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="img/navr.png"></button>',
     });
 
     $('div.pure-u-1-4').click(function (e) {
-      $('div.pure-u-1-4').removeClass('active-menu');
-      $(this).addClass('active-menu');
+        $('div.pure-u-1-4').removeClass('active-menu');
+        $(this).addClass('active-menu');
     });
+});
 
     
-  
-  });
-};
 
-
-
- 
+    
